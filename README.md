d435977dfc19548a5025c59e9287345096fbf943
Zubayer Hossain <zubayer94@gmail.com>
2021-01-04 12:39:29 +0600

    setup babel and vue-router

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# weather-app
+# weather-app-vuejs
 
 ## Project setup
 ```
---
+++ src/views/Weather.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sunt harum voluptas perferendis minima, eligendi neque unde at fuga odit optio deleniti distinctio rerum ab nobis aperiam impedit sit repudiandae.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
