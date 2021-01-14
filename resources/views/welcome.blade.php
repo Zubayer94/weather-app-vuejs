f993731bdf9ee2263a007d1b023c12e925b208b2
Zubayer Hossain <zubayer94@gmail.com>
2021-01-15 00:28:52 +0600

    Laravel 8 setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Weather app</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js" defer></script>
+</head>
+<body>
+    <div id="app">
+        <weather-component></weather-component>
+    </div>
+</body>
+</html>
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/getWeather', function (Request $request) {
+    // $response = Http::
+    return response()->json(['response' => 'Done!']);
+    // $cacheTime = date("h:i:sa");
+    // $cacheData = null;
+    // if ($cacheTime && $cacheTime > date("h:i:sa", strtotime("-30seconds"))) {
+    //     return response()->json(['response' => 'Cached Data!', 'data' => $cacheTime]);
+    // } else {
+    //     return response()->json(['response' => 'New Done!']);
+    // }
+    // Log::info(date("h:i:sa"));
+    // Log::info(date("h:i:sa", strtotime("-30seconds")));
+})->middleware('throttle:5,1');;
