9b80a1a8ae8dbc551c8e9d3f1dcf89a24caba958
Zubayer Hossain <zubayer94@gmail.com>
2021-02-08 03:17:45 +0600

    updated endpoint

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 window._ = require('lodash');
 window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+};
 window.Vue = require('vue').default;
 
 import Snotify, { SnotifyPosition } from 'vue-snotify'
