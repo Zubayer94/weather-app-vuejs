bdaf12fcd688ffeb75c1067e2de6a68512f06bd7
Zubayer Hossain <zubayer94@gmail.com>
2021-02-08 21:42:19 +0600

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Weather app</title>
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/logo/weather_favicon.png') }}" />
+    <link rel="shortcut icon" type="image/x-icon" href="img/logo/weather_favicon.png" />
     <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
     <link rel="stylesheet" href="/css/app.css">
     <script src="https://cdn.jsdelivr.net/npm/places.js@1.19.0" defer></script>
